68118d22c903399087066e3db51c858e94ff86d9
Wellington Viana <wellvsilva@gmail.com>
2019-10-11 20:55:57 -0300

    Utilizando unittest

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -7,7 +7,8 @@ Regras do Fizzbuzz
 4. Para qualquer outra posição fale o próprio número
 
 """
-from functools import  partial
+from functools import partial
+import unittest
 
 
 multiple_of = lambda base, num: num % base == 0
@@ -18,7 +19,7 @@ multiple_of_3 = partial(multiple_of, 3)
 def robot(pos):
     say = str(pos)
 
-    if multiple_of_3(pos) or multiple_of_5(pos):
+    if multiple_of_3(pos) and multiple_of_5(pos):
         say = 'fizzbuzz'
 
     elif multiple_of_5(pos):
@@ -30,31 +31,42 @@ def robot(pos):
     return say
 
 
-def assert_equal(result, expected):
-    from sys import _getframe
-    msg = 'Fail: Line {} got {} expecting {}'
+class FizzbuzzTest(unittest.TestCase):
+    def test_say_1_when_1(self):
+        self.assertEqual(robot(1), '1')
 
-    if not result == expected:
-        current = _getframe()
-        caller = current.f_back
-        line_no = caller.f_lineno
-        print(msg.format(line_no, result, expected))
-    
+    def test_say_2_when_2(self):
+        self.assertEqual(robot(2), '2')
+
+    def test_say_4_when_4(self):
+        self.assertEqual(robot(4), '4')
+
+    def test_say_3_when_3(self):
+        self.assertEqual(robot(3), 'fizz')
+
+    def test_say_6_when_6(self):
+        self.assertEqual(robot(6), 'fizz')
+
+    def test_say_9_when_9(self):
+        self.assertEqual(robot(9), 'fizz')
+
+    def test_say_5_when_5(self):
+        self.assertEqual(robot(5), 'buzz')
+
+    def test_say_10_when_10(self):
+        self.assertEqual(robot(10), 'buzz')
+
+    def test_say_20_when_20(self):
+        self.assertEqual(robot(20), 'buzz')
+
+    def test_say_15_when_15(self):
+        self.assertEqual(robot(15), 'fizzbuzz')
+
+    def test_say_30_when_30(self):
+        self.assertEqual(robot(30), 'fizzbuzz')
+
+    def test_say_45_when_45(self):
+        self.assertEqual(robot(45), 'fizzbuzz')
 
 if __name__ == '__main__':
-    assert_equal(robot(1), '1')
-    assert_equal(robot(2), '2')
-    assert_equal(robot(4), '4')
-
-    assert_equal(robot(3), 'fizz')
-    assert_equal(robot(6), 'fizz')
-    assert_equal(robot(9), 'fizz')
-
-    assert_equal(robot(5), 'buzz')
-    assert_equal(robot(10),  'buzz')
-    assert_equal(robot(20),  'buzz')
-                         
-    assert_equal(robot(15),  'fizzbuzz')
-    assert_equal(robot(30),  'fizzbuzz')
-    assert_equal(robot(45),  'fizzbuzz')
-
+    unittest.main()
