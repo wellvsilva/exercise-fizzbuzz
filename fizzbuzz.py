f65fc531f001a857a361a78edf6cc419db85df1c
Wellington Viana <wellvsilva@gmail.com>
2019-10-10 22:10:10 -0300

    Alterações dos testes do fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,15 +8,47 @@ Regras do Fizzbuzz
 
 """
 
+
+multiple_of = lambda base, num: num % base == 0
+
+
+def multiple_of_5(num):
+    return multiple_of(5, num)
+
+
+def multiple_of_3(num):
+    return multiple_of(3, num)
+
+
 def robot(pos):
-    return str(pos)
-    if pos == 4:
-        return str(pos)
-    if pos == 2:
-        return str(pos)
-    return str(pos)
+    say = str(pos)
+
+    if multiple_of_3(pos) and multiple_of_5(pos):
+        say = 'fizzbuzz'
+
+    elif multiple_of_5(pos):
+        say = 'buzz'
+
+    elif multiple_of_3(pos):
+        say = 'fizz'
+
+    return say
+
 
 if __name__ == '__main__':
     assert robot(1) == '1'
     assert robot(2) == '2'
     assert robot(4) == '4'
+
+    assert robot(3) == 'fizz'
+    assert robot(6) == 'fizz'
+    assert robot(9) == 'fizz'
+
+    assert robot(5) == 'buzz'
+    assert robot(10) == 'buzz'
+    assert robot(20) == 'buzz'
+
+    assert robot(15) == 'fizzbuzz'
+    assert robot(30) == 'fizzbuzz'
+    assert robot(45) == 'fizzbuzz'
+
